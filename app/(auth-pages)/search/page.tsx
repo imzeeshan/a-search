42a9f3c4d5d288441c731cf37d7021184224668b
Zeeshan Chawdhary <imzeeshanc@gmail.com>
2025-04-07 22:35:23 +0530

    Loading Indicator

--- app/(auth-pages)/search/page.tsx
+++ app/(auth-pages)/search/page.tsx
@@ -231,7 +231,17 @@ export default function Home() {
         </div>
         
         {/* Search results section */}
-        <SearchResults results={searchResults} />
+        {/* Loading indicator */}
+        {isLoading && (
+          <div className="flex justify-center items-center py-8">
+            <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
+          </div>
+        )}
+        
+        {/* Search Results */}
+        {!isLoading && searchResults.length > 0 && (
+          <SearchResults results={searchResults} />
+        )}
       </div>
     );
 }
